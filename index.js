e9052a1995f49d0445ba02599eb5c5d06855741f
Jonathan East <jonathan_east@ymail.com>
2020-12-12 16:09:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ class Cat {
 }
 
 class Dog {
-  
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof!`
+  }
 }
 
 class Bird {
